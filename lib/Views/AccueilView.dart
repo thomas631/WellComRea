f9d3d423f81d484992a243e32b2755baa2a4d454
gfriedtod <wilfried.wayou@2026.ucac-icam.com>
2024-06-06 14:25:59 +0100

    refactor AccueilView.dart

--- lib/Views/AccueilView.dart
+++ lib/Views/AccueilView.dart
@@ -15,65 +15,63 @@ class AccueilView extends StatelessWidget {
         ),
         backgroundColor: Colors.green.shade900,
       ),
-      body: Container(
-        child: Center(
-          child: Wrap(
-            spacing: 10,
-            direction: (size.width <= 700) ? Axis.vertical : Axis.horizontal,
-            children: <Widget>[
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => PatientView()),
-                  );
-                },
-                icon: const Icon(
-                  Icons.person,
-                  size: 40, // Ajustez la taille de l'icône ici
-                ), // Ajout de l'icône
-                label: const Text(
-                  'Espace Patient',
-                  style: TextStyle(fontSize: 30),
-                ),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.green.shade900,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  minimumSize: const Size(350, 250),
-                ),
+      body: Center(
+        child: Wrap(
+          spacing: 10,
+          direction: (size.width <= 700) ? Axis.vertical : Axis.horizontal,
+          children: <Widget>[
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PatientView()),
+                );
+              },
+              icon: const Icon(
+                Icons.person,
+                size: 40, // Ajustez la taille de l'icône ici
+              ), // Ajout de l'icône
+              label: const Text(
+                'Espace Patient',
+                style: TextStyle(fontSize: 30),
               ),
-
-              const SizedBox(width: 20), // Espace entre les boutons
-
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SoignantView()),
-                  );
-                },
-                icon: const Icon(
-                  Icons.medical_services,
-                  size: 40,
-                ), // Ajout de l'icône
-                label: const Text(
-                  'Espace Soignant',
-                  style: TextStyle(fontSize: 30),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.green.shade900,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.green.shade900,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  minimumSize: const Size(350, 250),
-                ),
+                minimumSize: const Size(350, 250),
               ),
-            ],
-          ),
+            ),
+
+            const SizedBox(width: 20), // Espace entre les boutons
+
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SoignantView()),
+                );
+              },
+              icon: const Icon(
+                Icons.medical_services,
+                size: 40,
+              ), // Ajout de l'icône
+              label: const Text(
+                'Espace Soignant',
+                style: TextStyle(fontSize: 30),
+              ),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.green.shade900,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                minimumSize: const Size(350, 250),
+              ),
+            ),
+          ],
         ),
       ),
     );
